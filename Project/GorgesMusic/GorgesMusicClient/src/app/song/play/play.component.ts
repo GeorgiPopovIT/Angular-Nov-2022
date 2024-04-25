77f1aa7c4a1f0ccdd464f904c663c8cb2c865cb4
GeorgiPopovIT <gogopopo26062003@gmail.com>
2024-04-26 00:03:30 +0300

    Fixed when click new song, audio player reload and play the new song.

--- Project/GorgesMusic/GorgesMusicClient/src/app/song/play/play.component.ts
+++ Project/GorgesMusic/GorgesMusicClient/src/app/song/play/play.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges,OnInit, SimpleChanges, signal } from '@angular/core';
+import { Component, Input, OnChanges,OnDestroy,OnInit, SimpleChanges, signal } from '@angular/core';
 import { SongService } from 'src/app/services/song.service';
 import { Song } from 'src/app/shared/interfaces/song';
 import { ActivatedRoute } from '@angular/router';
@@ -9,16 +9,19 @@ import { BehaviorSubject, Subject } from 'rxjs';
   templateUrl: './play.component.html',
   styleUrls: ['./play.component.css']
 })
-export class PlayComponent  implements  OnChanges  {
+export class PlayComponent  implements OnChanges  {
 
   @Input()
   public songToPlay! : Song;
+  public songUrl = signal(this.songToPlay);
 
   constructor(private songService : SongService,private route : ActivatedRoute)
    {}
   
- 
   ngOnChanges(changes: SimpleChanges): void {
+    if(changes['songToPlay'] !== undefined){
+       this.songUrl.update(v =>v = changes['songToPlay'].currentValue);
+       document.getElementsByTagName('audio')[0].load();
+    }
   }
-
 }
